aad53c8fffdbb4696039241c68b7242c9f2c6524
Joakim Svensson <48772061+Aeico@users.noreply.github.com>
2024-11-14 16:45:07 +0000

    context search and country_codes

--- GDELT.py
+++ GDELT.py
@@ -4,15 +4,22 @@ import json
 #Example
 #"https://api.gdeltproject.org/api/v2/doc/doc?query=%22News%22%20sourcecountry:US%20sourcelang:English&format=json"
 
-# TODO: look for other modes, fix max records, 
-def fetch_gdelt_data(query_term="News", source_country="US", source_lang="English", mode="artlist"):
-    base_url = "https://api.gdeltproject.org/api/v2/doc/doc"
+# Context might be ideal only issue is designating origin country.
+# Resource: https://blog.gdeltproject.org/announcing-the-gdelt-context-2-0-api/
+def fetch_gdelt_data(query_term="News", source_country="China", source_lang="English", mode="artlist"):
+    #base_url = "https://api.gdeltproject.org/api/v2/doc/doc"
+    base_url = "https://api.gdeltproject.org/api/v2/context/context"
     
-    query = f'"{query_term}" sourcecountry:{source_country} sourcelang:{source_lang}'
+    #query = f'"{query_term}" sourcecountry:{source_country} sourcelang:{source_lang}'
+    query = f'"{query_term}"'
     
     params = {
+        'format': "json",
+        'timespan': "72H",
         'query': query,
-        'format': "json"
+        'searchlang': "english",
+        'maxrecords':200,
+        'sort':"DateDesc",
     }
 
     headers = { # Was needed for 429 error, might look for other solution
@@ -21,8 +28,9 @@ def fetch_gdelt_data(query_term="News", source_country="US", source_lang="Englis
     
     # Send GET request
     response = requests.get(base_url, params=params, headers=headers)
-    
+    print(response.url)
     if response.status_code == 200:
+        
         data = json.loads(response.text)
         return data
     else:
@@ -32,6 +40,6 @@ def fetch_gdelt_data(query_term="News", source_country="US", source_lang="Englis
 # Test usage
 # TODO:Need to decide on what generic search terms should be...
 if __name__ == "__main__":
-    data = fetch_gdelt_data(query_term="Biden", source_country="US", source_lang="English", mode="artlist")
+    data = fetch_gdelt_data(query_term="jinping", source_country="China", source_lang="English", mode="artlist")
     if data:
         print(json.dumps(data, indent=2))
