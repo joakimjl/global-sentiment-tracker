1343a115047d0a9e375092bc3ee2588981753699
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2024-11-24 15:27:25 +0000

    New queries to figure out selection of ideal sites

--- domain_info_queries.sql
+++ domain_info_queries.sql
@@ -17,6 +17,22 @@ DROP TABLE domain_info
 
 SELECT * FROM domain_info
 
-SELECT count(domain) 
-FROM domain_info
-WHERE domain_weight != 0
+SELECT di.*
+FROM domain_info di,
+UNNEST(di.country_mentions) AS cm(country_code, count)
+WHERE di.domain_weight > 0.5
+  AND cm.count > 1500
+GROUP BY di.domain
+
+
+SELECT di.*
+FROM domain_info di,
+UNNEST(di.country_mentions) AS cm(country_code, count)
+WHERE di.domain_weight > 0.5
+  AND cm.count > 100000
+  AND cm.country_code = 'CH'
+GROUP BY di.domain
+
+
+
+SELECT count(country_code) FROM domain_info
