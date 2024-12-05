9dc4686264c6142836f0bd0c12b2f979824da1d2
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2024-12-05 12:38:22 +0000

    Planet works, click on release.

--- front-end/main.js
+++ front-end/main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { randInt } from 'three/src/math/MathUtils';
+import { clamp, randInt } from 'three/src/math/MathUtils';
+import { TessellateModifier } from 'three/examples/jsm/modifiers/TessellateModifier.js';
+import * as d3 from "d3";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -8,7 +10,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var doneBefore = 0;
 const radius = 2;
 
 fetch('/UN_Geodata_simplified.geojson')
@@ -17,17 +18,11 @@ fetch('/UN_Geodata_simplified.geojson')
         geojsonData.features.forEach(feature => {
             const coordType = feature.geometry.type;
 
-            console.log(feature)
             if ( (coordType == "MultiPolygon" || coordType == "Polygon") && feature.properties.nam_en != null){
                 
                 const coordinateSet = feature.geometry.coordinates;
                 const randColor = randInt(0,999999);
-                console.log(coordinateSet[0])
                 for (let index = 0; index < coordinateSet.length; index++) {
-                    if (doneBefore >= 999) {
-                        continue;
-                    }
-                    
                     
                     var coordinates = coordinateSet[index][0];
                     if (coordType == "Polygon") {
@@ -35,50 +30,51 @@ fetch('/UN_Geodata_simplified.geojson')
                     }
                     var tempCoords = [];
                     
-                    
                     for (let index = 0; index < coordinates.length; index++) {
                         const coordsArr = coordinates[index];
                         tempCoords.push(coordsArr);
                     }
                     coordinates = tempCoords;
-
-                    const shapePoints = coordinates
-                        .map(([lon, lat]) => latLonToVector3(lat, lon, radius))
-                        .filter(point => !isNaN(point.x) && !isNaN(point.y) && !isNaN(point.z));
-
                     
                     const shape = new THREE.Shape();
-                    /* const geometry = new THREE.BufferGeometry();
-                    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( triangleCoords, 3 ) ); */
-
-                    shape.moveTo( coordinates[0][0], coordinates[0][1])
-                    for( var i = 2; i < coordinates.length; i++ ){
-                        shape.lineTo( coordinates[i][0], coordinates[i][1], coordinates[i-1][0], coordinates[i-1][1], coordinates[i-2][0], coordinates[i-2][1]);
+                    shape.moveTo(coordinates[0][0], coordinates[0][1]);
+                    for (let i = 1; i < coordinates.length; i++) {
+                        shape.lineTo(coordinates[i][0], coordinates[i][1]);
                     }
 
                     const extrudeSettings = {
                         steps: 1,
-                        depth: 0.1,
+                        depth: 0.001,
                         bevelEnabled: true,
-                        bevelThickness: 0.05,
+                        bevelThickness: 0.02,
                         bevelSize: 0.05,
                         bevelOffset: 0,
-                        bevelSegments: 1
+                        bevelSegments: 1,
+                        curveSegments: 8
                     };
 
-                    const geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-                    
-                    geometry.computeBoundingSphere();
-                    
-                    const material = new THREE.MeshBasicMaterial({color: randColor});
-                    const country = new THREE.Mesh( geometry, material );
+                    let geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
 
-                    /* const material = new THREE.LineBasicMaterial({ color: 0x9d00ff });
-                    const country = new THREE.Line(geometry, material); */
+                    let tesselateAmount = clamp(coordinates.length/2,5,100);
+                    const tessellateModifier = new TessellateModifier(3,tesselateAmount);
+                    geometry = tessellateModifier.modify(geometry);
+
+                    for (let i = 0; i < geometry.attributes.position.count; i++) {
+                        const x = geometry.attributes.position.getX(i);
+                        const y = geometry.attributes.position.getY(i);
+                        const z = geometry.attributes.position.getZ(i);
+
+                        const newXYZ = latLonToVector3(y, x, radius + z);
+                        geometry.attributes.position.setXYZ(i, newXYZ[0], newXYZ[1], newXYZ[2]);
+                    }
+                    geometry.computeBoundingSphere();
+
+                    const material = new THREE.MeshPhysicalMaterial({ color: randColor, side: THREE.DoubleSide});
+
+                    const country = new THREE.Mesh(geometry, material);
+                    country.name = feature.properties.iso2cd
                     scene.add(country);
-                    
                 }
-                doneBefore += 1;
             }
         });
     });
@@ -87,60 +83,61 @@ function latLonToVector3(lat, lon, radius) {
     const phi = (90 - lat) * (Math.PI / 180);
     const theta = (-lon + 180) * (Math.PI / 180);
 
-    return new THREE.Vector3(
+    return [
         radius * Math.sin(phi) * Math.cos(theta),
         radius * Math.cos(phi),
-        radius * Math.sin(phi) * Math.sin(theta)
-    );
+        radius * Math.sin(phi) * Math.sin(theta)];
 }
 
-const geometrySphere = new THREE.SphereGeometry( 1.96, 64, 64 ); 
+const geometrySphere = new THREE.SphereGeometry( 2, 64, 64 ); 
 const materialSphere = new THREE.MeshPhysicalMaterial( { color: 0x3030ff } ); 
 const sphere = new THREE.Mesh( geometrySphere, materialSphere );
 
-const pointLight = new THREE.PointLight({ color: 0xffffff }, 30,100);
-pointLight.position.x = 9;
-pointLight.position.z = 9;
+for (let index = 0; index < 3; index++) {
+    const newLight = new THREE.PointLight({color: 0xFFFFFF}, 50, 30);
+    let val = 5;
+    if (index == 0){
+        val = -5;
+    } else if (index == 1){
+        val = 0;
+    }
+    newLight.position.x = 10;
+    newLight.position.y = val;
+    newLight.position.z = 10;
+    newLight.name = "Light"
+    scene.add(newLight);
+}
 
-scene.add(pointLight);
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
-const pointLight1 = new THREE.PointLight({ color: 0xffffff }, 30,100);
-pointLight1.position.x = -9;
-pointLight1.position.z = -9;
-
-scene.add(pointLight1);
-
-const pointLight2 = new THREE.PointLight({ color: 0xffffff }, 30,100);
-pointLight2.position.x = -9;
-pointLight2.position.z = 9;
-
-scene.add(pointLight2);
-
-const pointLight3 = new THREE.PointLight({ color: 0xffffff }, 30,100);
-pointLight3.position.x = 9;
-pointLight3.position.z = -9;
-
-scene.add(pointLight3);
-
-const geometry = new THREE.BufferGeometry();
-
-const positions = [
-0, 0, 0,    // v1
-0, 1, 0,   // v2
-0, 1, 1  // v3
-];
-
-geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
-geometry.computeVertexNormals();
-
-const object = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
-scene.add(object);
+function onPointerMove( event ) {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
 
 camera.position.z = 5;
 const controls = new OrbitControls(camera, renderer.domElement);
+scene.add(sphere);
+
+var intersects = raycaster.intersectObjects( scene.children );
+var hoveredMesh;
 
 function animate() {
+    raycaster.setFromCamera( pointer, camera );
+    intersects = raycaster.intersectObjects( scene.children );
+    if (intersects.length >= 1){
+        hoveredMesh = intersects[0].object;
+    } else {
+        hoveredMesh = "None"
+    }
     requestAnimationFrame(animate);
+    scene.rotateY(0.0001);
     renderer.render(scene, camera);
 }
 animate();
+window.addEventListener( 'pointermove', onPointerMove );
+
+window.addEventListener('click', (e) => {
+    console.log(hoveredMesh)
+});
