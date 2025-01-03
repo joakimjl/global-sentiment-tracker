7d33a611e350e94967fcd3d47fb6ffd8c60fd3f3
Aeico <eojwolf@gmail.com>
2025-01-03 15:13:50 +0100

    Extra delay for slow system

--- GDELT.py
+++ GDELT.py
@@ -650,7 +650,7 @@ def fetch_and_insert_one(target, subject, remain_rows, roberta, syncer, on_day=d
             return False
          
         print(f"Working on insert for {target}")
-        time.sleep(random.random()*60) #For slower systems
+        time.sleep(random.random()*200) #For slower systems
         
         sentiment_arr_nat, titles_nat, target_country, query = process_titles(
                 target_country=target, date=on_day, roberta=roberta, syncer=syncer, titles=titles_nat) 
