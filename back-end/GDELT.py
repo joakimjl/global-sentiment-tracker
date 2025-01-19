8c0ee4fd48cd99250ca9bfb7fc494226d69c3765
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-01-19 23:48:27 +0000

    Another conflict fixed

--- back-end/GDELT.py
+++ back-end/GDELT.py
@@ -911,15 +911,9 @@ def run_all(in_datetime, boolean_map = {"dump":True, "insert":False, "fetch_new"
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     boolean_map = {"dump":False, "insert":False, "fetch_new":False, "upload":True, "process":True, "connected":False, "download_processed":False}
     #on_datetime = datetime(year=2025, month=1, day=16, hour=0, minute=0, second=0)
     #run_all(on_datetime, boolean_map)
     if boolean_map['upload'] == True:
         S3BatchHandler().upload_processed("temp_processed")
     
-=======
-    boolean_map = {"dump":True, "insert":False, "fetch_new":True}
-    on_datetime = datetime(year=2025, month=1, day=16, hour=0, minute=0, second=0)
-    run_all(on_datetime, boolean_map)
->>>>>>> 2faa109 (Now works dumping from 16'th)
