755dd30bac831c636c7b71101037d4cdfe630d42
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-01-20 00:34:58 +0000

    Main set, just needs threads

--- back-end/GDELT.py
+++ back-end/GDELT.py
@@ -934,7 +934,7 @@ def run_all(in_datetime, boolean_map = {"dump":True, "insert":False, "fetch_new"
 if __name__ == "__main__":
     #boolean_map = {"dump":False, "insert":False, "fetch_new":False, "upload":True, "process":True, "connected":False, "download_processed":False} #For upload
     #boolean_map = {"dump":False, "insert":True, "fetch_new":False, "upload":False, "process":False, "connected":True, "download_processed":True} #Downloading processed
-    boolean_map = {"dump":True, "insert":True, "fetch_new":False, "upload":False, "process":False, "connected":True, "download_processed":True} #Fetch and upload info
+    boolean_map = {"dump":True, "insert":False, "fetch_new":True, "upload":True, "process":False, "connected":True, "download_processed":False} #Fetch and upload info
     on_datetime = []
     for i in range(5):
         on_datetime.append(datetime(year=2025, month=1, day=16, hour=4+4*i, minute=0, second=0))
