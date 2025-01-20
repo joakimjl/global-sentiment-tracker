9476b4a77cc11aef03ef5f43fe86d1424b9f236d
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-01-20 14:42:44 +0000

    config for Processing

--- back-end/GDELT.py
+++ back-end/GDELT.py
@@ -848,6 +848,8 @@ def fetch_and_insert_one(target, subject, remain_rows, roberta, syncer, on_day=d
 
 def run_all(in_datetime, boolean_map = {"dump":True, "insert":False, "fetch_new":True}):
     lock = ProcessLock(allowed_amount=1)
+    if boolean_map['connected'] == False:
+        lock = ProcessLock(allowed_amount=160)
     #lock.disableLock()
     #If parallell is better run with lock on disable ( lock.disableLock() )
     syncer = TranslatorSyncer()
@@ -932,13 +934,13 @@ def run_all(in_datetime, boolean_map = {"dump":True, "insert":False, "fetch_new"
 
 
 if __name__ == "__main__":
-    #boolean_map = {"dump":False, "insert":False, "fetch_new":False, "upload":True, "process":True, "connected":False, "download_processed":False} #For upload
+    boolean_map = {"dump":False, "insert":False, "fetch_new":False, "upload":True, "process":True, "connected":False, "download_processed":False} #For upload and processing
     #boolean_map = {"dump":False, "insert":True, "fetch_new":False, "upload":False, "process":False, "connected":True, "download_processed":True} #Downloading processed
-    boolean_map = {"dump":True, "insert":False, "fetch_new":True, "upload":True, "process":False, "connected":True, "download_processed":False} #Fetch and upload info
+    #boolean_map = {"dump":True, "insert":False, "fetch_new":True, "upload":True, "process":False, "connected":True, "download_processed":False} #Fetch and upload info
     on_datetime = []
-    #for i in range(5):
-    #    on_datetime.append(datetime(year=2025, month=1, day=16, hour=4+4*i, minute=0, second=0))
-    #run_all(on_datetime, boolean_map)
+    for i in range(5):
+        on_datetime.append(datetime(year=2025, month=1, day=16, hour=4+4*i, minute=0, second=0))
+    run_all(on_datetime, boolean_map)
     if boolean_map['fetch_new'] == True and boolean_map['upload'] == True:
         S3BatchHandler().zip_batch("temp_articles")
     elif boolean_map['upload'] == True and boolean_map['fetch_new'] == False:
