f11619e9796f4404d3f8cebe43a3024c99a88fbc
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-01-20 14:32:31 +0000

    Upload ready

--- back-end/GDELT.py
+++ back-end/GDELT.py
@@ -936,9 +936,9 @@ if __name__ == "__main__":
     #boolean_map = {"dump":False, "insert":True, "fetch_new":False, "upload":False, "process":False, "connected":True, "download_processed":True} #Downloading processed
     boolean_map = {"dump":True, "insert":False, "fetch_new":True, "upload":True, "process":False, "connected":True, "download_processed":False} #Fetch and upload info
     on_datetime = []
-    for i in range(5):
-        on_datetime.append(datetime(year=2025, month=1, day=16, hour=4+4*i, minute=0, second=0))
-    run_all(on_datetime, boolean_map)
+    #for i in range(5):
+    #    on_datetime.append(datetime(year=2025, month=1, day=16, hour=4+4*i, minute=0, second=0))
+    #run_all(on_datetime, boolean_map)
     if boolean_map['fetch_new'] == True and boolean_map['upload'] == True:
         S3BatchHandler().zip_batch("temp_articles")
     elif boolean_map['upload'] == True:
