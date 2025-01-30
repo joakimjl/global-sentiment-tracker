6f2ee3ca315471bbeef4f5511bdcddf11a9401fe
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-01-30 14:57:45 +0000

    Hour -> Day Formatting

--- back-end/GDELT.py
+++ back-end/GDELT.py
@@ -942,7 +942,7 @@ if __name__ == "__main__":
     boolean_map = {"dump":True, "insert":False, "fetch_new":True, "upload":True, "process":False, "connected":True, "download_processed":False} #Fetch and upload info
     on_datetime = []
     for i in range(7):
-        on_datetime.append(datetime(year=2025, month=1, day=17, hour=4+4*i, minute=0, second=0))
+        on_datetime.append(datetime(year=2025, month=1, day=17+int( (4+4*i)/24 ), hour=(4+4*i)%24, minute=0, second=0))
     run_all(on_datetime, boolean_map)
     if boolean_map['fetch_new'] == True and boolean_map['upload'] == True:
         S3BatchHandler().zip_batch("temp_articles",day=date(year=2025, month=1, day=17))
