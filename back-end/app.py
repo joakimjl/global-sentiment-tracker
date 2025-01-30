b056b87ee2509ebe86b3475be0ad14fb868b92b3
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-01-30 19:56:00 +0000

    Test 2

--- back-end/app.py
+++ back-end/app.py
@@ -44,11 +44,12 @@ def fetch_sentiment(country,query,timeframe):
     FROM global_info_hourly\
     GROUP BY target_country")
     res = cur.fetchall()
+    temp_res = []
     for ele in res:
-        temp = (country_codes_map[ele[0]],ele[1],ele[2],ele[3],ele[4])
-        ele = temp
+        temp = [country_codes_map[ele[0]],ele[1],ele[2],ele[3],ele[4]]
+        temp_res.append(temp)
     #Data format is country, vader national, roberta national, vader international, roberta international
-    return json.dumps(res)
+    return json.dumps(temp_res)
 
 def create_server():
     return app
