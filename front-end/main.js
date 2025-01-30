b7f707e5fc49cfbccb0927e0823552236471afe5
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-01-30 16:06:02 +0000

    Cors again

--- front-end/main.js
+++ front-end/main.js
@@ -122,6 +122,9 @@ async function fetchQuery(country, query, timeframe) {
             headers: {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Origin" : '*',
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+                "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
             },
         });
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
