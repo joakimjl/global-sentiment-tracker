85b19aec604182134fa8f9521ff3abcdbdb08501
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-02-11 22:21:56 +0000

    allows timeframe

--- back-end/app.py
+++ back-end/app.py
@@ -31,7 +31,26 @@ def hello_info():
     except:
         timeframe = 1
 
-    return fetch_sentiment(country,query,timeframe)
+    return fetch_sentiment_timeframe(country,query,timeframe)
+
+def fetch_sentiment_timeframe(country,query,timeframe):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SELECT target_country,\
+    sum(senti_count_nat[1]) as vader_nat,\
+    sum(senti_count_nat[2]) as rober_nat,\
+    sum(senti_count_int[1]) as vader_int,\
+    sum(senti_count_int[2]) as rober_int,\
+    on_time \
+    FROM global_info_hourly\
+    GROUP BY target_country,on_time")
+    res = cur.fetchall()
+    temp_res = []
+    for ele in res:
+        temp = [country_codes_map[ele[0]],ele[1],ele[2],ele[3],ele[4]]
+        temp_res.append(temp)
+    #Data format is country, vader national, roberta national, vader international, roberta international
+    return json.dumps(temp_res)
 
 def fetch_sentiment(country,query,timeframe):
     conn = connect()
