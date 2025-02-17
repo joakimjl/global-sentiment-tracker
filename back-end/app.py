b36928384ebd77d0c696010ec4ade1342ef58e30
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-02-17 18:36:59 +0000

    Now str

--- back-end/app.py
+++ back-end/app.py
@@ -113,7 +113,7 @@ def fetch_word_data():
     on_time \
     FROM global_info_hourly \
     WHERE target_country = %s AND on_time >= %s AND on_time <= %s\
-    GROUP BY target_country, on_time",(str(country),day,dayAfter))
+    GROUP BY target_country, on_time",(str(country),day.fromtimestamp,dayAfter.fromtimestamp))
 
     res = cur.fetchall()
     temp_res = []
