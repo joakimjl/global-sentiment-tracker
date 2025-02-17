ebc605bb7e6f6da3cb767ba3f2991492dfb63c93
EC2 Default User <ec2-user@ip-172-31-55-132.eu-west-1.compute.internal>
2025-02-17 21:40:53 +0000

    backed functional words query

--- back-end/app.py
+++ back-end/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS, cross_origin
 import json
 from country_codes import country_codes_map
 import datetime as datetime
+import nltk
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -108,17 +109,64 @@ def fetch_word_data():
 
     dayAfter = day+datetime.timedelta(days=1)
 
-    cur.execute("SELECT target_country,\
-    UNNEST(headline_inter), \
-    UNNEST(senti_count_int), \
+    cur.execute("SELECT\
     on_time, \
+    UNNEST(headline_inter), \
+    UNNEST(sentiment_inter[2:2]) as bert \
     FROM global_info_hourly \
     WHERE target_country = %s AND on_time >= %s AND on_time <= %s\
-    GROUP BY target_country, on_time",(str(country),day,dayAfter))
-
+    GROUP BY target_country, on_time, headline_inter, sentiment_count_res",(str(country),day,dayAfter))
+    
     res = cur.fetchall()
     temp_res = []
-    return json.dumps([res])
+    for ele in res:
+        temp_arr = []
+        temp_arr.append(ele[0].strftime("%Y-%m-%d-%H"))
+        temp_arr.append(ele[1])
+        temp_arr.append(ele[2])
+        temp_res.append(temp_arr)
+    resWords = impact_words(json.dumps([temp_res]))
+    return json.dumps(resWords)
+
+
+def impact_words(jsonInfo):
+    loadJson = json.loads(jsonInfo)
+    loadJson = loadJson[0]
+    allWords = {}
+
+    tokenizer = nltk.NLTKWordTokenizer()
+
+    for i in range(len(loadJson)):
+        stringFix = "[" + loadJson[i][2][1:-1] + "]"
+        sentiment = json.loads(stringFix)[2] - json.loads(stringFix)[0]
+        processed = loadJson[i][1].split('"')
+        sentence = tokenizer.tokenize(processed[1])
+
+        for pair in nltk.pos_tag(sentence):
+            if pair[1] == "NNP" and len(pair[0]) >= 2:
+                if allWords.get(pair[0]) == None:   
+                    allWords[pair[0]] = [1,sentiment]
+                else:
+                    allWords[pair[0]][0] += 1
+                    allWords[pair[0]][1] += sentiment
+
+    allWords = {k: v for k, v in sorted(allWords.items(), key=lambda item: item[1][1])}
+
+    posWords = []
+    negWords = []
+
+    i = 0
+
+    for ele in allWords:
+        if i < 8:
+            posWords.append([ele, allWords[ele]])
+        if i+8 >= len(allWords):
+            negWords.append([ele, allWords[ele]])
+        i+=1
+    print(negWords)
+    print(posWords)
+    return[posWords,negWords]
+
 
 def create_server():
     return app
