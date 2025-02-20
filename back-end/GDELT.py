da49da2a5e92eca59c06016b58869c1fea642af8
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-02-20 12:50:17 +0000

    Clearer comments

--- back-end/GDELT.py
+++ back-end/GDELT.py
@@ -958,9 +958,9 @@ def run_all(in_datetime, boolean_map = {"dump":True, "insert":False, "fetch_new"
 
 if __name__ == "__main__":
     start_time_total = time.time()
-    boolean_map = {"dump":False, "insert":False, "fetch_new":False, "upload":True, "process":True, "connected":False, "download_processed":False} #For upload and processing
-    #boolean_map = {"dump":False, "insert":True, "fetch_new":False, "upload":False, "process":False, "connected":True, "download_processed":True} #Downloading processed
-    #boolean_map = {"dump":True, "insert":False, "fetch_new":True, "upload":True, "process":False, "connected":True, "download_processed":False} #Fetch and upload info
+    boolean_map = {"dump":False, "insert":False, "fetch_new":False, "upload":True, "process":True, "connected":False, "download_processed":False} #GPU Processing and upload
+    #boolean_map = {"dump":False, "insert":True, "fetch_new":False, "upload":False, "process":False, "connected":True, "download_processed":True} #Insert processed from S3
+    #boolean_map = {"dump":True, "insert":False, "fetch_new":True, "upload":True, "process":False, "connected":True, "download_processed":False} #Fetch, translate and upload info
     day = 1
     month = 2
     year = 2025
